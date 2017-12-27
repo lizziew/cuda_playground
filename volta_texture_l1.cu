a41c593fe70ae2a36789e0c8bff2834b0bdb8c9c
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2017-12-27 00:24:52 -0800

    refactored and cleaned up volta texture benchmarking

--- volta_texture_l1.cu
+++ volta_texture_l1.cu
@@ -1,6 +1,8 @@
-// Fine grained measurement of texture L1 cache
-// Tesla V100-SXM2-16GB
-// source code based on paper https://arxiv.org/pdf/1509.02308.pdf
+/*
+   Finds: C, the cache size 
+   For the Tesla V100-SXM2-16GB's texture L1 cache 
+   Soure code based on paper https://arxiv.org/pdf/1509.02308.pdf 
+*/ 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,172 +10,152 @@
 // array size is 24576 * 4 / 1024 = 96 KB       
 #define ARR_SIZE 24576
 
+// number of iterations 
+#define ITER 4096
+
 //declare the texture
 texture<int, 1, cudaReadModeElementType> tex_ref; 
 
-__global__ void texture_latency (int * my_array, int size, unsigned int *duration, int *index, int iter, unsigned int INNER_ITS ) {
+__global__ void texture_latency (int * my_array, int size, unsigned int *duration, int *index, int iter) {
+  // data access latencies array
+  __shared__ unsigned int s_tvalue[ITER];
+  // accessed data indices array
+  __shared__ int s_value[ITER];
 
-   const int it =  4096;
+  // initialize arrays
+  for (int i = 0; i < ITER; i++) {
+    s_value[i] = -1; 
+    s_tvalue[i] = 0; 
+  }
+  
+  unsigned int start, end;
+  int j = 0;
+  for (int k = 0; k <= iter; k++) {
+    for (int cnt = 0; cnt < ITER; cnt++) { 
+      start = clock();
 
-
-   __shared__ unsigned int s_tvalue[it];
-   __shared__ int s_value[it];
-
-   unsigned int start, end;
-   int i,j;
-
-   //initilize j	
-   j=0;
-
-	for (i=0; i< it; i++) {
-	s_value[i] = -1; 
-	s_tvalue[i]=0;
-}
-
-   for (int k=0; k <= iter; k++) {
-
-	for (int cnt=0; cnt < it; cnt++) {
-			
-			start=clock();
-			j=tex1Dfetch(tex_ref, j);
-			s_value[cnt] = j;
-			
-			end=clock();			
-			s_tvalue[cnt] = (end -start);
-			}
-
-	
-
+      // traverse an array whose elements are initialized as the indices for the next memory access
+      j = tex1Dfetch(tex_ref, j);
+      // handles ILP with this data dependency 
+      s_value[cnt] = j;
+      
+      end = clock();			
+      s_tvalue[cnt] = end - start;
+    }
   }
 
-  for (i=0; i< it; i++){
-	duration[i] = s_tvalue[i];
-	index[i] = s_value[i];
+  for (int i = 0; i < ITER; i++) {
+	  duration[i] = s_tvalue[i];
+	  index[i] = s_value[i];
 	}
 
-	my_array[size] = i;
-	my_array[size+1] = s_tvalue[i-1];
+	my_array[size] = ITER;
+	my_array[size+1] = s_tvalue[ITER-1];
 }
-
-
 
 void parametric_measure_texture(int N, int iterations, int stride) {
 	cudaError_t error_id;
+  
+  // host (CPU) array
+	int* h_a;
 
-	int * h_a, * d_a;
-	int size =  N * sizeof(int);
-	h_a = (int *)malloc(size);
-	//initialize array	
+  int size =  N * sizeof(int);
+  h_a = (int*) malloc(size); 
+  
 	for (int i = 0; i < (N-2); i++) {
 		h_a[i] = (i + stride) % (N-2);
 	}
 	h_a[N-2] = 0;
 	h_a[N-1] = 0;
-	cudaMalloc ((void **) &d_a, size);
-	//copy it to device array
-	cudaMemcpy((void *)d_a, (void *)h_a, size, cudaMemcpyHostToDevice);
 
-	// here to change the iteration numbers 
-	unsigned int INNER_ITS = 16;
-	const int it = INNER_ITS*256;
-	
-	// the time ivformation array and index array
-	unsigned int *h_duration = (unsigned int *)malloc(it*sizeof(unsigned int));
-	int *h_index = (int *)malloc(it*sizeof(int));
-	
-	int *d_index;
-	 error_id = cudaMalloc(&d_index,  it*sizeof(int));
+  // device (GPU array)
+  int* d_a;
+ 	cudaMalloc((void **) &d_a, size);
+	cudaMemcpy((void*) d_a, (void*) h_a, size, cudaMemcpyHostToDevice);
+
+  // accessed data indices array on host (CPU)
+	int *h_index = (int*) malloc(ITER * sizeof(int));	
+  // data access latencies array on host (CPU)
+	unsigned int *h_duration = (unsigned int*) malloc(ITER * sizeof(unsigned int));
+
+  // accessed data indices array on device (GPU)
+	int* d_index;
+	error_id = cudaMalloc(&d_index, ITER * sizeof(int));
 	if (error_id != cudaSuccess) {
-		printf("Error 1.1 is %s\n", cudaGetErrorString(error_id));
+		printf("Error from allocating indices array is %s\n", cudaGetErrorString(error_id));
 	}
 
+  // data access latencies array on device (GPU)
 	unsigned int *d_duration;
-	 error_id = cudaMalloc(&d_duration,  it*sizeof(unsigned int));
+	error_id = cudaMalloc(&d_duration, ITER * sizeof(unsigned int));
 	if (error_id != cudaSuccess) {
-		printf("Error 1.2 is %s\n", cudaGetErrorString(error_id));
+		printf("Error from allocating latencies array is %s\n", cudaGetErrorString(error_id));
 	}
 
-
-	//bind texture
-	cudaBindTexture(0, tex_ref, d_a, size );
-
-	cudaThreadSynchronize ();
-
-        error_id = cudaGetLastError();
-        if (error_id != cudaSuccess) {
-		printf("Error 2 is %s\n", cudaGetErrorString(error_id));
+	// before a kernel can use a texture reference to read from texture memory, 
+  // the texture reference must be bound to a texture 
+	cudaBindTexture(0, tex_ref, d_a, size);
+  
+  // blocks until the device has completed all preceding requested tasks
+	cudaThreadSynchronize();
+  error_id = cudaGetLastError();
+  if (error_id != cudaSuccess) {
+		printf("Error from binding texture is %s\n", cudaGetErrorString(error_id));
 	}
 
-	//for (int l=0; l < 20; l++) {
+  // 1 x 1 block of threads
+  dim3 Db = dim3(1);
+  // 1 x 1 x 1 block of threads
+  dim3 Dg = dim3(1,1,1);
+  // launch kernel
+  texture_latency<<<Dg, Db>>>(d_a, size, d_duration, d_index, iterations);
 
-		// launch kernel
-		dim3 Db = dim3(1);
-		dim3 Dg = dim3(1,1,1);
-		texture_latency <<<Dg, Db>>>(d_a, size, d_duration, d_index, iterations, INNER_ITS);
+  cudaThreadSynchronize();
+  error_id = cudaGetLastError();
+  if (error_id != cudaSuccess) {
+    printf("Error from launching kernel is %s\n", cudaGetErrorString(error_id));
+  }
 
-		cudaThreadSynchronize ();
+  cudaThreadSynchronize();
 
-		error_id = cudaGetLastError();
-		if (error_id != cudaSuccess) {
-			printf("Error 3 is %s\n", cudaGetErrorString(error_id));
-		}
+	// copy results from GPU to CPU 
+	cudaMemcpy((void*) h_index, (void*) d_index, ITER * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy((void*) h_duration, (void*) d_duration, ITER * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
-		cudaThreadSynchronize ();
-
-	/* copy results from GPU to CPU */
-	cudaMemcpy((void *)h_index, (void *)d_index, it*sizeof(int) , cudaMemcpyDeviceToHost);
-	cudaMemcpy((void *)h_duration, (void *)d_duration, it*sizeof(unsigned int) , cudaMemcpyDeviceToHost);
-
-	//}
-
-	//print the result
-	printf("\n=====Visting the %f KB array, loop %d*%d times======\n", (float)(N-2)*sizeof(int)/1024.0f, it, 1);
-	for (int i=0;i<it;i++){	
-		printf("%10d\t %10f\n", h_index[i], (float)h_duration[i]);
+	// print the resulting benchmarks
+	printf("\n=====Visting the %f KB array, loop %d*%d times======\n", (float)(N-2)*sizeof(int)/1024.0f, ITER, 1);
+	for (int i = 0; i < ITER; i++){	
+		printf("%10d\t %10f\n", h_index[i], (float) h_duration[i]);
 	}
 
-
-	//unbind texture
+	// unbind texture
 	cudaUnbindTexture(tex_ref);
 
-	//free memory on GPU
+	// free memory on GPU
 	cudaFree(d_a);
 	cudaFree(d_duration);
 	cudaFree(d_index);
-        cudaThreadSynchronize ();
+  cudaThreadSynchronize();
 	
 	// free memory on CPU
-        free(h_a);
-        free(h_duration);
+  free(h_a);
+  free(h_duration);
 	free(h_index);
-	
 }
 
 int main() {
-	
 	cudaSetDevice(0); // current device 
 
-	int N, iterations, stride;
-
-	iterations = 1;
+  // repeatedly executed this amount of times
+	int iterations = 1;
+  // array sequentially traversed with this amount
+  int stride = 1;
 	
-	// measure L1, should be 96 KB
-
-	// stage 1: overflow with 1 element
-	stride = 1; // in element
-	for (N = ARR_SIZE; N <= ARR_SIZE; N += stride) {
+	for (int N = ARR_SIZE; N <= ARR_SIZE; N += stride) {
+    // last 2 elements of array contain special values
 		parametric_measure_texture(N+2, iterations, stride);
 	}
 
-/*
-	// stage2: overflow with cache lines
-	stride = 8; // in element
-	for (N = 3072; N <=3200; N += stride) {
-		parametric_measure_texture(N+2, iterations, stride);
-	} 
-*/
-
-	cudaDeviceReset();
+	cudaDeviceReset(); // destroy context
 	return 0;
 
-}
