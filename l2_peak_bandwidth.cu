2eb3ccf856505300895dd088e80da9dbacde64fc
lizziew <elizabeth.m.wei@gmail.com>
2018-02-13 23:01:24 -0500

    parallelize

--- l2_peak_bandwidth.cu
+++ l2_peak_bandwidth.cu
@@ -11,10 +11,10 @@ nvprof -m l2_write_throughput ./peak
 # include <stdint.h>
 # include "cuda_runtime.h"
 
-__global__ void global_latency (unsigned int* my_array, int N, int iterations) {
+__global__ void global_latency (unsigned int* a, unsigned int* b, unsigned int* c, int N, int iterations) {
   for (int k = 0; k < iterations; k++) {
-    for (int j = 0; j < N-2; j++) {
-       my_array[j+2] = my_array[j+1] + my_array[j];
+    for (int j = 0; j < N; j++) {
+       c[j] = a[j] + b[j];
     }
   }
 }
@@ -22,21 +22,33 @@ __global__ void global_latency (unsigned int* my_array, int N, int iterations) {
 void parametric_measure_global(int N, int iterations) {
   cudaDeviceReset();
   
-  // host (CPU) array
-  unsigned int *h_a;
-  h_a = (unsigned int *) malloc(sizeof(unsigned int) * N);
+  // host (CPU) arrays
+  unsigned int *ha0;
+  unsigned int *ha1;
+  unsigned int *ha2;
+  ha0 = (unsigned int*) malloc(sizeof(unsigned int) * N);
+  ha1 = (unsigned int*) malloc(sizeof(unsigned int) * N);
+  ha2 = (unsigned int*) malloc(sizeof(unsigned int) * N);
   
-  // device (GPU) array
-  unsigned int *d_a;
-  cudaMalloc((void**) &d_a, sizeof(unsigned int) * N);
+  // device (GPU) arrays
+  unsigned int *da0;
+  unsigned int *da1;
+  unsigned int *da2;
+  cudaMalloc((void**) &da0, sizeof(unsigned int) * N);
+  cudaMalloc((void**) &da1, sizeof(unsigned int) * N);
+  cudaMalloc((void**) &da2, sizeof(unsigned int) * N);
 
   // initialize host (CPU) array 
   for (int i = 0; i < N; i++) {   
-    h_a[i] = 0; 
+    ha0[i] = 1; 
+    ha1[i] = 1;
+    ha2[i] = 0;
   }
 
   // copy array from host to device
-  cudaMemcpy(d_a, h_a, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(da0, ha0, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(da1, ha1, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(da2, ha2, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
 
   cudaThreadSynchronize();
  
@@ -49,12 +61,9 @@ void parametric_measure_global(int N, int iterations) {
   // launch kernel with a large number of threads to saturate bandwidth 
   dim3 Db = dim3(32,32,1);
   dim3 Dg = dim3(512,1,1);
-  global_latency <<<Dg, Db>>>(d_a, N, iterations);
+  global_latency <<<Dg, Db>>>(da0, da1, da2, N, iterations);
   cudaEventRecord(stop);
-
   cudaThreadSynchronize();
-
-  cudaEventSynchronize(stop);
 
   cudaError_t error_id = cudaGetLastError();
     if (error_id != cudaSuccess) {
@@ -68,20 +77,24 @@ void parametric_measure_global(int N, int iterations) {
   printf("time in ms: %f\n", milliseconds);
 
   // free memory on GPu
-  cudaFree(d_a);
+  cudaFree(da0);
+  cudaFree(da1);
+  cudaFree(da2);
     
   // free memory on CPU 
-  free(h_a);
+  free(ha0);
+  free(ha1);
+  free(ha2);
   
   cudaDeviceReset();  
 }
 
 void measure_global() {
-  // access 3 MB array 1000s of times
-  int iterations = 100;
-  int N = 3 * 1024* 1024/sizeof(unsigned int); // 3MB
+  // access 3 1 MB arrays 1000s of times
+  int iterations = 1;
+  int N = 1024* 1024/sizeof(unsigned int); // 1 MB
   
-  printf("\n=====%ld MB array * %d times====\n", sizeof(unsigned int)*N/1024/1024, iterations);
+  printf("\n=====3 %ld MB arrays * %d times====\n", sizeof(unsigned int)*N/1024/1024, iterations);
   parametric_measure_global(N, iterations);
 }
 
